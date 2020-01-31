86866696d1577cd478b1ad228eb0a07583e14481
GabrielGQA <gabrielgqa@gmail.com>
2020-01-31 12:26:43 -0300

    configura sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: '12345',
+  database: 'db_fastfeet',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
