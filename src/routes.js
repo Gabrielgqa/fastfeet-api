8dcfaae00702cf8a972f273bc31b1a230b772cba
GabrielGQA <gabrielgqa@gmail.com>
2020-02-04 12:05:29 -0300

    inserção de recipients

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
+import RecipientController from './app/controllers/RecipientController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -13,5 +14,7 @@ routes.get('/', (req, res) => {
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+
+routes.post('/recipients', RecipientController.store);
 
 export default routes;
